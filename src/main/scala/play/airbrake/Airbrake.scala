f0d32b7e914b478f713dce33abf0e60c6582f892
Adam Lane <enalmada@gmail.com>
2013-03-11 00:09:13 -0700

    Update Airbrake to play 2.1
    I really wanted to use Airbrake with play 2.1 so this is the code that I ended up with after much toil. 
    I am not a scala programmer and play 2.1 did change exceptions stuff so I am sure this could be done better but at least it works enough in 2.1 to use and be a starting point to make it work the right way.

--- src/main/scala/play/airbrake/Airbrake.scala
+++ src/main/scala/play/airbrake/Airbrake.scala
@@ -1,56 +1,62 @@
 package play.airbrake
 
 import play.api._
-import play.api.PlayException.UsefulException
+import play.api.PlayException
 import play.api.mvc.RequestHeader
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Akka
 import play.api.Play.current
 import scala.collection.JavaConversions._
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.UnexpectedException
+import scala.Some
+
 
 object Airbrake {
-  private lazy val app = implicitly[Application]
+  private lazy val app = play.api.Play
   private lazy val enabled = app.configuration.getBoolean("airbrake.enabled") getOrElse { Play.isProd }
-  private lazy val apiKey = app.configuration.getString("airbrake.apiKey") getOrElse { throw PlayException("Configuration error", "Could not find airbrake.apiKey in settings") }
+  private lazy val apiKey = app.configuration.getString("airbrake.apiKey") getOrElse { throw UnexpectedException() }
   private lazy val ssl = app.configuration.getBoolean("airbrake.ssl").getOrElse(false)
 
   /**
-    * Scala API
-    *
-    * {{{
-    * // app/Global.scala
-    * override def onError(request: RequestHeader, ex: Throwable) = {
-    *   Airbrake.notify(request, ex)
-    *   super.onError(request, ex)
-    * }
-    * }}}
-    */
+   * Scala API
+   *
+   * {{{
+   * // app/Global.scala
+   * override def onError(request: RequestHeader, ex: Throwable) = {
+   *   Airbrake.notify(request, ex)
+   *   super.onError(request, ex)
+   * }
+   * }}}
+   */
   def notify(request: RequestHeader, th: Throwable): Unit = if(enabled) _notify(request.method, request.uri, request.session.data, th)
 
   /**
-    * Java API
-    *
-    * {{{
-    * // app/Global.java
-    * @Override
-    * public Result onError(RequestHeader request, Throwable t) {
-    *   Airbrake.notify(request, t);
-    *   return super.onError(request, t);
-    * }
-    * }}}
-    */
+   * Java API
+   *
+   * {{{
+   * // app/Global.java
+   * @Override
+   * public Result onError(RequestHeader request, Throwable t) {
+   *   Airbrake.notify(request, t);
+   *   return super.onError(request, t);
+   * }
+   * }}}
+   */
   def notify(request: play.mvc.Http.RequestHeader, th: Throwable): Unit = if(enabled){
-    val data = request.headers.toMap.mapValues(_.toString)
+    val data = request.headers.toMap.mapValues(_.head)
     _notify(request.method, request.uri, data, th)
   }
 
   protected def _notify(method: String, uri: String, data: Map[String, String], th: Throwable): Unit =
+  {
     Akka.future {
       val scheme = if(ssl) "https" else "http"
-      WS.url(scheme + "://api.airbrake.io/notifier_api/v2/notices").post(formatNotice(app, apiKey, method, uri, data, liftThrowable(th))).onRedeem { response =>
+      WS.url(scheme + "://api.airbrake.io/notifier_api/v2/notices").post(formatNotice(app, apiKey, method, uri, data, liftThrowable(th))).onComplete { response =>
         Logger.info("Exception notice sent to Airbrake")
       }
     }
+  }
 
 
   def js = if(enabled) { """
@@ -61,16 +67,16 @@ object Airbrake {
       Airbrake.setEnvironment(%s);
       Airbrake.setGuessFunctionName(true);
     </script>
-    """.format(apiKey, app.mode)
+                         """.format(apiKey, app.mode)
   } else ""
 
   protected def liftThrowable(th: Throwable) = th match {
-    case e: PlayException.UsefulException => e
+    case e: PlayException => e
     case e => UnexpectedException(unexpected = Some(e))
   }
 
-  protected def formatNotice(app: Application, apiKey: String, method: String, uri: String, data: Map[String,String], ex: UsefulException) = {
-    <notice version="2.2">
+  protected def formatNotice(app: play.api.Play.type, apiKey: String, method: String, uri: String, data: Map[String,String], ex: UsefulException) = {
+    <notice version="2.3">
       <api-key>{ apiKey }</api-key>
       <notifier>
         <name>play-airbrake</name>
@@ -81,7 +87,7 @@ object Airbrake {
         <class>{ ex.title }</class>
         <message>{ ex.description }</message>
         <backtrace>
-          { ex.cause.toList.flatMap(e => formatStacktrace(e.getStackTrace)) }
+          { ex.cause.getStackTrace.toList.flatMap(e => formatStacktrace(e)) }
         </backtrace>
       </error>
       <request>
@@ -101,18 +107,18 @@ object Airbrake {
     else Nil
 
   protected def formatVars(vars: Map[String, String]) = vars.map { case (key, value) =>
-    <var key={ key }>{ value }</var>
+    <var key={ key }>{  value }</var>
   }
 
   protected def formatTitle(ex: UsefulException) =
-    ex.title + ex.cause.map(e => " :: " + e.getClass.getName).getOrElse("")
+    ex.title + ex.cause.getStackTrace.map(e => " :: " + e.getClass.getName)
 
   protected def formatMessage(ex: UsefulException) =
-    ex.description + ex.cause.map(e => "\n---\n" + e.getMessage).getOrElse("")
+    ex.description + ex.cause.getStackTrace.map(e => "\n---\n" + e.getMethodName)
 
-  protected def formatStacktrace(trace: Array[StackTraceElement]) = trace.flatMap { e =>
+  protected def formatStacktrace(e: StackTraceElement) = {
     val line = "%s.%s(%s)" format (e.getClassName, e.getMethodName, e.getFileName)
-    <line file={line} number={e.getLineNumber.toString}/>
+      <line file={line} number={e.getLineNumber.toString}/>
   }
 
 }
